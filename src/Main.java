8629f8f4b082567e32d434583a30386b3a7d2e68
ibrahim <moussaibrahimcoulibaly2@gmail.com>
2026-05-01 19:04:44 +0000

    Ajouter la méthode main avec les tests des compteurs

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Personne personne1 = new Personne();
+        Personne personne2 = new Personne();
+        Personne personne3 = new Personne();
+        Personne personne4 = new Personne();
+
+        // Affichez les valeurs des deux compteurs
+        System.out.println("(" + personne4.nbLocal + "," + Personne.nbInstances + ")");
     }
 }
